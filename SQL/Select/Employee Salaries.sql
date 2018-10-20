1aabaf4b6ac4fdbfdf96240ae6b290ad56938c96
Yu Han Huang <40597903+floraxhuang@users.noreply.github.com>
2018-10-20 16:16:18 -0400

    Create Employee Salaries.sql

---
+++ SQL/Select/Employee Salaries.sql
@@ -0,0 +1,18 @@
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+Input Format
+The Employee table containing employee data for a company is described as follows:
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| employee_id | INTEGER    |
+| name        | STRING     |
+| months      | INTEGER    |
+| salary      | INTEGER    |
++-------------+------------+
+where employee_id is an employee's ID number, name is their name, months is the total number of months they've been working for the company, and salary is their monthly salary.
+*/
+
+SELECT name FROM Employee
+WHERE salary>2000 and months<10
+ORDER BY employee_id ASC;
