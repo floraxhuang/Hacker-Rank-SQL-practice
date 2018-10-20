870a84cb1bf4de391d7677f85592f3b73f516893
Yu Han Huang <40597903+floraxhuang@users.noreply.github.com>
2018-10-20 15:00:40 -0400

    Create Select All.sql

---
+++ SQL/Select/Select All.sql
@@ -0,0 +1,14 @@
+/*
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| Name        | varchar2(17) |
+| CountryCode | varchar2(3)  |
+| District    | varchar2(20) |
+| Population  | NUMBER       |
++-------------+--------------+
+Query all columns (attributes) for every row in the CITY table.
+*/
+
+SELECT * FROM CITY;
