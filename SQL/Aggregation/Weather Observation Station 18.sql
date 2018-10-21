2a458d719a4ed6df066feee561d8257659f4ec6b
Yu Han Huang <40597903+floraxhuang@users.noreply.github.com>
2018-10-21 00:30:28 -0400

    Create Weather Observation Station 18.sql

---
+++ SQL/Aggregation/Weather Observation Station 18.sql
@@ -0,0 +1,12 @@
+/*
+Consider  and  to be two points on a 2D plane.
+
+ happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+*/
+
+SELECT round(abs(min(LAT_N)-max(LAT_N)) + abs(min(LONG_W)-max(LONG_W)),4)
+FROM STATION
