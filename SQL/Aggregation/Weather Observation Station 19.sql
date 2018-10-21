3f3187e89fcf92cbef54185181b98102eaaa63fa
Yu Han Huang <40597903+floraxhuang@users.noreply.github.com>
2018-10-21 00:35:12 -0400

    Create Weather Observation Station 19.sql

---
+++ SQL/Aggregation/Weather Observation Station 19.sql
@@ -0,0 +1,11 @@
+/*
+Consider  and  to be two points on a 2D plane where  are the respective minimum and maximum values of Northern Latitude (LAT_N) and  are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+
+Query the Euclidean Distance between points  and  and format your answer to display  decimal digits.
+
+Input Format
+
+The STATION table is described as follows: */
+
+SELECT ROUND(POWER(POWER(MAX(LAT_N)-MIN(LAT_N),2)+POWER(MAX(LONG_W)-MIN(LONG_W),2),0.5),4)
+FROM STATION
